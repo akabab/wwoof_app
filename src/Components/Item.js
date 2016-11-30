bc5b36cc8dbd24e5e29163ce63c1e5fd08f41930
Yoann Cribier <ycribier@student.42.fr>
2016-11-30 16:09:31 +0100

    css

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -10,8 +10,10 @@ class Item extends Component {
       <div className="item">
         <div className="item-title">{title}</div>
         <div className="item-address">{address}</div>
-        <div className="item-since">since: {wwoof_since}</div>
-        <div className="item-activity">activity: {activity || "none provided"}</div>
+        <div className="item-activity">
+          <div className="item-activity-label">activities:</div>
+          <div className="item-activity-value">{activity || "none provided"}</div>
+        </div>
         <div className="item-description">{description}</div>
       </div>
     )
