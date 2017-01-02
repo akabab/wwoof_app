0187a195e928d99ef8ea5d40ccd7796e99924dbd
Yoann Cribier <ycribier@student.42.fr>
2017-01-02 22:30:52 +0100

    Item - added some displayed props

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 import './Item.css'
+import _ from 'lodash'
+
+const absoluteUrl = (url) => (!/^(?:f|ht)tps?:\/\//.test(url)) ? "http://" + url : url
+const prettyUrl = (url) => url.replace(/^(?:f|ht)tps?:\/\//, "")
 
 class Item extends Component {
 
   render() {
-    const { title, address, activity, description } = this.props.item
+    const {
+      id,
+      title,
+      address,
+      activity,
+      description,
+      mails,
+      mobiles,
+      phones,
+      websites,
+    } = this.props.item
+
+    const mailLinks = (mails || []).map(m => <a key={m} href={"mailto:"+m}>{m}</a>)
+    const phoneLinks = _.concat(mobiles || [], phones || []).map(p => <a key={p} href={"tel:"+p}>{p}</a>)
+    const websitesLinks = (websites || []).map(w => <a key={absoluteUrl(w)} href={w}>{prettyUrl(w)}</a>)
 
     return (
       <div className="item">
@@ -15,6 +33,11 @@ class Item extends Component {
           <div className="item-activity-value">{activity || "none provided"}</div>
         </div>
         <div className="item-description">{description}</div>
+        <div className="item-contacts">
+          <div className="item-contacts-mails">mails: {mailLinks}</div>
+          <div className="item-contacts-phones">phones: {phoneLinks}</div>
+          <div className="item-contacts-websites">websites: {websitesLinks}</div>
+        </div>
       </div>
     )
   }
